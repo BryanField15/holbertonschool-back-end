c0f95b7ee59f36df1acae50b809d70f60efd5896
BryanField15 <6367@holbertonstudents.com>
2023-08-20 21:16:28 -0700

    task 0 solution

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Script that shows the progress of a to do list"""
+import requests
+import sys
+
+if __name__ == "__main__":
+
+    url = "https://jsonplaceholder.typicode.com/"
+    user_todos = requests.get(f"{url}users/{sys.argv[1]}/todos").json()
+    user_info =  requests.get(f"{url}users/{sys.argv[1]}").json()
+
+    total_todos = len(user_todos)
+    completed_todos = 0
+    for todo in user_todos:
+        if todo.get("completed") == True:
+            completed_todos += 1
+
+    user_name = user_info.get("name")
+
+    print(f"Employee {user_name} is done with tasks({completed_todos}/{total_todos}):")
+    for todo in user_todos:
+        if todo.get("completed") == True:
+            print(f"\t {todo['title']}")
