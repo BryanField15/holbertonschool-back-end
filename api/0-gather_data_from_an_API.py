f17fa498a3203e9847bea4c9fe31c5be1c74bc59
BryanField15 <6367@holbertonstudents.com>
2023-08-20 21:22:23 -0700

    pycodes

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -7,17 +7,18 @@ if __name__ == "__main__":
 
     url = "https://jsonplaceholder.typicode.com/"
     user_todos = requests.get(f"{url}users/{sys.argv[1]}/todos").json()
-    user_info =  requests.get(f"{url}users/{sys.argv[1]}").json()
+    user_info = requests.get(f"{url}users/{sys.argv[1]}").json()
 
     total_todos = len(user_todos)
     completed_todos = 0
     for todo in user_todos:
-        if todo.get("completed") == True:
+        if todo.get("completed"):
             completed_todos += 1
 
     user_name = user_info.get("name")
+    print(f"Employee {user_name} is done with "
+          f"tasks({completed_todos}/{total_todos}):")
 
-    print(f"Employee {user_name} is done with tasks({completed_todos}/{total_todos}):")
     for todo in user_todos:
-        if todo.get("completed") == True:
+        if todo.get("completed"):
             print(f"\t {todo['title']}")
